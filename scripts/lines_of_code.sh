90087240a6823611f796dc0e4b433a66a9ff1be4
Niklas Vainio <niklasvainio8@gmail.com>
2026-02-01 00:57:04 -0800

    Added lines of code script

---
+++ scripts/lines_of_code.sh
@@ -0,0 +1,6 @@
+# Print out the total number of lines of code in .cpp and .hpp files
+NUM_LINES_OF_CODE=$(find . \( -path './build' -o -path './external' \) -prune -o -type f \( -name '*.cpp' -o -name '*.hpp' \) -print0 | xargs -0 cat | wc -l)
+
+echo
+echo "Total lines of code: $NUM_LINES_OF_CODE"
+echo
