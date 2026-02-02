4e01102aa1ee51b4fee7ebf0bb70ad9adb4ea775
Niklas Vainio <niklasvainio8@gmail.com>
2026-02-01 19:02:35 -0800

    Fixed lines of code script

--- scripts/lines_of_code.sh
+++ scripts/lines_of_code.sh
@@ -1,5 +1,5 @@
 # Print out the total number of lines of code in .cpp and .hpp files
-NUM_LINES_OF_CODE=$(find . \( -path './build' -o -path './external' \) -prune -o -type f \( -name '*.cpp' -o -name '*.hpp' \) -print0 | xargs -0 cat | wc -l)
+NUM_LINES_OF_CODE=$(find . \( -path './build' -o -path './external' -o -path './scripts/generated' \) -prune -o -type f \( -name '*.cpp' -o -name '*.hpp' \) -print0 | xargs -0 cat | wc -l)
 
 echo
 echo "Total lines of code: $NUM_LINES_OF_CODE"
